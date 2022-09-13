6785eabecfab9d6f80bc03fbb361ddef923e423c
SydVic <fabrizio.bocca27@gmail.com>
2022-09-13 15:28:24 +0200

    aggiunto carousel 3d con lavori boolean

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,94 @@
 <template>
   <div id="app">
 
+    <!-- HEADER -->
+    <AppHeader/>
+    <!-- /HEADER -->
+
+    <!-- CAROUSEL 3D -->
+    <carousel-3d>
+      <slide v-for="(object, index) in projects" :key="index" :index="index">
+        <img :src="require(`./assets/projects-images/${object.image}`)" alt="">
+      </slide>
+    </carousel-3d>
+    <!-- /CAROUSEL 3D -->
+
   </div>
 </template>
 
 <script>
+  import AppHeader from "./components/AppHeader.vue";
+  import Vue from 'vue';
+  import Carousel3d from 'vue-carousel-3d';
 
-export default {
-  name: 'App',
-  components: {
+  Vue.use(Carousel3d);
 
+  export default {
+    name: 'App',
+    components: {
+      AppHeader,
+    },
+    data: function() {
+      return {
+        projects : [
+        {
+            title: "Bool B&B",
+            image: "bool-b&b.jpg",
+          },
+          {
+            title: "Playstation",
+            image: "playstation.jpg",
+          },
+          {
+            title: "Boolflix",
+            image: "boolflix.jpg",
+          },
+          {
+            title: "DC-Comics",
+            image: "comics.jpg",
+          },
+          {
+            title: "Azienda Spedizioni",
+            image: "spedizioni.jpg",
+          },
+          {
+            title: "Boolzap",
+            image: "boolzap.jpg",
+          },
+          {
+            title: "Dropbox",
+            image: "dropbox.jpg",
+          },
+          {
+            title: "Discord",
+            image: "discord.jpg",
+          },
+          {
+            title: "Spotify",
+            image: "spotify.jpg",
+          },
+        ]
+      }
+    }
   }
-}
 </script>
 
 <style lang="scss">
+  @import "./style/_variables.scss";
 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background-color: $primary;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <!-- HEADER -->
+  <section class="header">
+    <!-- CONTAINER -->
+    <div class="container">
+      <!-- NAV LINKS -->
+      <ul class="nav-links">
+        <li>
+          <a href="">
+            ABOUT ME
+          </a>
+        </li>
+        <li>
+          <a href="">
+            WORKS
+          </a>
+        </li>
+      </ul>
+      <!-- NAV LINKS -->
+    </div>
+    <!-- /CONTAINER -->
+  </section>
+  <!-- /HEADER -->
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../style/variables.scss";
+
+  .header {
+    width: 100%;
+    height: $header-height;
+    background-color: $primary;
+
+    .container {
+      width: $container-width;
+      height: 100%;
+      margin: 0 auto;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      .nav-links {
+        list-style: none;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+
+        li {
+          margin: 0 1rem;
+          a {
+            text-decoration: none;
+            color: $secondary;
+            padding: .3rem;
+          }
+        }
+      }
+    }
+  }
+</style>
